b533bfa5fa0b60c5a36de93e6fbe46a24e257eb5
McYeremia <117885019+McYeremia@users.noreply.github.com>
2023-02-15 19:27:36 +0700

    Add files via upload

---
+++ 4_D_71220953.py
@@ -0,0 +1,14 @@
+while True:
+    print('\t !! Selamat datang di H+ GYM !!')
+    print('Silahkan pilih menu dibawah ini: \n 1. Menambah data \n 2. Menampilkan data \n 3. Keluar')
+    pil1 = int(input('Silahkann masukan pilihan yang anda inginkan: '))
+    
+    if pil1 == 1:
+        pel= (input('Masukan Nama Pelanggan:'))
+        mem =(input('Masukan Jenis Member: '))
+        print('Data berhasil ditambahkan!')
+    elif pil1 == 2:
+        print(f'Pelanggan\t Member: \n {pel}\t {mem}')
+
+    elif pil1 == 3:
+        break
